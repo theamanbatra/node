48bb6dd90a8f61f268cbeb72394034d00ff1bd0f
Sahib Productions <sahibproductions@Sahibs-MacBook-Pro.local>
2021-08-02 20:05:16 +0530

    turn

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,16 @@ myVideo.muted = true
 //     host: 'localhost',
 //     port: '3001'
 // })
-const myPeer = new Peer()
+
+const myPeer = new Peer({ key: 'oz9b3ni30qtcsor', debug: 3, config: {'iceServers': [
+    { url: 'stun:stun.l.google.com:19302' },
+    { url: 'turn:numb.viagenie.ca:3478', credential: 'muazkh', username:'web...@live.com' },
+    { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username:'web...@live.com' },
+    { url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' },
+    { url: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' }
+]}});
+
+// const myPeer = new Peer()
 const peers = {}
 
 let typing = false;
